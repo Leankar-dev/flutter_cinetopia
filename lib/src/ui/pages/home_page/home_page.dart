b639f35243a53c2a5a4f29380450ebedbf1c9c51
Leandro <l.silva01@uol.com.br>
2025-05-01 16:01:58 +0100

    Correcting the spacing and adding Gradient to the Home Page.

--- lib/src/ui/pages/home_page/home_page.dart
+++ lib/src/ui/pages/home_page/home_page.dart
@@ -7,33 +7,46 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(title: Center(child: const Text('Cinetopia App'))),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Image.asset('assets/logo.png'),
-            Image.asset('assets/splash.png'),
-            const Text(
-              'O lugar ideal para você assistir seus filmes e séries favoritos',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
+      body: Ink(
+        // usar o Ink em vez do Container para o botão não desaparecer
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xFF000000), Color(0xFF1D0E44)],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 26),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/logo.png'),
+              const SizedBox(height: 24),
+              Image.asset('assets/splash.png'),
+              const SizedBox(height: 24),
+              Text(
+                'O lugar ideal para você assistir seus filmes e séries favoritos',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
               ),
-            ),
-            const SizedBox(height: 20),
-            CustomPrimaryButtonWidget(
-              onTap: () {},
-              text: 'Quero Começar',
-              textColor: Color(0xFF1D0E44),
-              fontWeight: FontWeight.bold,
-              fontSize: 20,
-              backgroundColor: Color(0xFFB370FF),
-              iconData: Icons.arrow_forward,
-              iconSize: 20,
-            ),
-          ],
+              const SizedBox(height: 24),
+              CustomPrimaryButtonWidget(
+                onTap: () {},
+                text: 'Quero Começar',
+                textColor: Color(0xFF1D0E44),
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+                backgroundColor: Color(0xFFB370FF),
+                iconData: Icons.arrow_forward,
+                iconSize: 20,
+              ),
+            ],
+          ),
         ),
       ),
     );
